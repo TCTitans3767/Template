f52f426621094061741d9d48b0159ec9cdf3ea57
Fandit <indephisive@gmail.com>
2025-10-16 20:28:56 -0400

    Initial commit

---
+++ src/main/java/frc/robot/subsystems/arm/ArmIO.java
@@ -0,0 +1,3 @@
+package frc.robot.subsystems.arm;
+
+public interface ArmIO {}
---
+++ src/main/java/frc/robot/subsystems/robotControl/RobotControl.java
@@ -0,0 +1,24 @@
+package frc.robot.subsystems.robotControl;
+
+import RobotController.RobotController;
+import org.littletonrobotics.junction.Logger;
+
+public class RobotControl extends RobotController implements RobotControlIO{
+    private final RobotControlIOInputsAutoLogged inputs = new RobotControlIOInputsAutoLogged();
+    private final RobotControlIO io = this;
+
+    @Override
+    public void periodic() {
+        updateInputs(inputs);
+        Logger.processInputs("RobotControl", inputs);
+    }
+
+    @Override
+    public void updateInputs(RobotControlIOInputs inputs) {
+        inputs.currentCommand = currentMode.getName();
+        inputs.previousCommand = previousMode.getName();
+        inputs.currentDriveCommand = currentDriveMode.getName();
+        inputs.previousDriveCommand = previousDriveMode.getName();
+        inputs.currentCommandRunning = currentMode.isScheduled();
+    }
+}
